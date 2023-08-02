69161f5c446a653ff2de659a382095e30ea13850
Cristian Cavanzo <cristian.frontendev@gmail.com>
2023-08-02 13:51:38 -0500

    [Add]: intial commit

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	semi: true,
+	singleQuote: true,
+	tabWidth: 2,
+	useTabs: true,
+	trailingComma: 'es5',
+	bracketSpacing: true,
+	printWidth: 100,
+};
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+	return (
+		<div>
+			<p className="uppercase">Hola</p>
+		</div>
+	);
+};
+
+export default Home;
