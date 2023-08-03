326d69d818803626047ec0d72499e40f5be1f6c1
Cristian Cavanzo <cristian.frontendev@gmail.com>
2023-08-03 05:40:29 -0500

    [Add]: onClick

--- src/components/Card/CardImages.tsx
+++ src/components/Card/CardImages.tsx
@@ -7,7 +7,7 @@ const CardImages = ({ image }: Props) => {
 	return (
 		<div className="relative w-80 h-80">
 			<div className="absolute top-0 left-0 right-0 bottom-0">
-				<Image src={image} fill alt="Image" className="object-cover rounded-md" />
+				<Image src={image} fill alt="Image" className="object-cover rounded-md" loading="lazy" />
 			</div>
 		</div>
 	);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
 import { CardImages } from 'components/Card/CardImages';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import type { MouseEventHandler } from 'react';
+type Images = { id: string; url: string };
 interface State {
-	images: { id: string; url: string }[];
+	images: Images[];
 }
 const Home = () => {
 	const [state, setStates] = useState<State>({ images: [] });
-	useEffect(() => {
+	const addFox: MouseEventHandler<HTMLButtonElement> = (event) => {
+		event.preventDefault();
 		const random = (): number => Math.floor(Math.random() * 124);
 		const image = `https://randomfox.ca/images/${random()}.jpg`;
 		const newStateImages = state.images;
@@ -14,10 +17,14 @@ const Home = () => {
 			url: image,
 		});
 		setStates({ ...state, images: newStateImages });
-	}, []);
+	};
+
 	return (
 		<div>
 			<p className="uppercase">Hola</p>
+			<button className="rounded-md bg-blue-950 text-white py-2 px-4 border-none" onClick={addFox}>
+				Agregar
+			</button>
 			{state.images.map((image) => (
 				<div key={image.id} className="p-4">
 					<CardImages image={image.url} />
